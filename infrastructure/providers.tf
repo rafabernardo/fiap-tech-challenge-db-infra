e0912b086ce71140a6872151e62497c18f6f7e8f
Rafaela Rodrigues <rafaela@highnote.io>
2024-12-08 20:33:55 -0300

    Update Terraform configuration by adding .terraform.lock.hcl, removing unused resources, and adjusting provider versions

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.24"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
