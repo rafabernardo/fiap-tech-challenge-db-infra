1b182fd8260e0121c98df8833df191409c1839f3
Rafaela Rodrigues <rafaela@highnote.io>
2024-12-15 17:00:23 -0300

    Remove unnecessary Terraform configuration for provider and backend settings

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,4 @@
-terraform {
-  required_version = ">= 0.13"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.80.0"
-    }
-  }
-  backend "s3" {
-    bucket = "fiap-tech-challenge-terraform"
-    key    = "fiap-tech-challenge-terraform-db/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 
 provider "aws" {
   region = var.aws_region
